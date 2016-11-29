64badb443284a9a095589862923a98ed146afea1
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-29 13:59:44 +0000

    adding drink and empty

--- specs/water_bottle_spec.js
+++ specs/water_bottle_spec.js
@@ -2,11 +2,25 @@ var waterBottle = require('../water_bottle');
 var assert = require('assert');
 
 describe( "Water Bottle", function() {
+
   it( "should be empty at start", function() {
     assert.equal( 0, waterBottle.volume );
   });
+
   it( "should go to 100 when filled", function(){
     waterBottle.fill();
     assert.equal( 100, waterBottle.volume );
+  });
+
+  it( "should go down by 10 when drank", function() {
+    waterBottle.fill();
+    waterBottle.drink();
+    assert.equal( 90, waterBottle.volume );
+  });
+
+  it( "should go to 0 when emptied", function() {
+    waterBottle.fill();
+    waterBottle.empty();
+    assert.equal( 0, waterBottle.volume );
   });
 });
--- water_bottle.js
+++ water_bottle.js
@@ -2,6 +2,12 @@ var watterBottle = {
   volume: 0,
   fill: function() {
     this.volume = 100;
+  },
+  drink: function() {
+    this.volume -= 10;
+  },
+  empty: function() {
+    this.volume = 0;
   }
 };
 
