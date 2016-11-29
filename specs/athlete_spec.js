e53c235f642e5734812b0da3286cacf7cf65096e
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-29 14:30:57 +0000

    adding test for run

--- specs/athlete_spec.js
+++ specs/athlete_spec.js
@@ -2,12 +2,18 @@ var assert = require("assert");
 var athlete = require("../athlete");
 
 describe("Athlete", function(){
-  
+
   it( "hydration should start at 100", function(){
     assert.equal( 100, athlete.hydration );
   });
 
   it( "should have distance starting at 0", function() {
     assert.equal( 0, athlete.distance );
+  });
+
+  it( "run increases distance reduces hydration", function(){
+    athlete.run();
+    assert.equal(10, athlete.distance );
+    assert.equal(90, athlete.hydration );
   })
 });
