41b1355344dc8a711781abbce6827bb276e54771
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-29 14:37:49 +0000

    Athlete does not move when hydration is 0 test passed

--- athlete.js
+++ athlete.js
@@ -1,6 +1,12 @@
 var athlete = {
   hydration: 100,
-  distance: 0
+  distance: 0,
+  run: function() {
+    if (this.hydration >= 10){
+      this.hydration -= 10;
+      this.distance += 10;
+    }
+  }
 };
 
 module.exports = athlete;
--- specs/athlete_spec.js
+++ specs/athlete_spec.js
@@ -16,4 +16,13 @@ describe("Athlete", function(){
     assert.equal(10, athlete.distance );
     assert.equal(90, athlete.hydration );
   })
+
+  it( "athlete does not move when hydration is 0", function() {
+    for ( var i = 0; i < 100; i++ ) {
+      athlete.run();
+    }
+
+    assert.equal( 0, athlete.hydration );
+    assert.equal( 100, athlete.distance );
+  })
 });
